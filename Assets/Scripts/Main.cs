f9d36f909d215eb0fc785c386d2d21c2f49ef48c
madalenassembleia <madalena.assembleia@gmail.com>
2018-05-22 16:26:02 +0100

    Modelo da casa

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -8,7 +8,7 @@ public class Main : MonoBehaviour
     public void Play()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
 
